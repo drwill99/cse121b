1acc524df91d1e74593b2eb4782ea465f757bc8d
drwill99 <drw98@byui.edu>
2024-01-17 13:52:39 -0800

    idk dude

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,14 +4,21 @@
 
 /* Step 2 - Variables */
 
+let fullName = "Dallin Williams"
 
+let currentYear = Date
 
+let profilePicture = images/mask.jpg
 
 /* Step 3 - Element Variables */
 
+const nameElement = document.getElementById('name');
 
+const foodElement = document.getElementById('food');
 
+const yearElement = document.querySelector('#year');
 
+const imageElement = document.querySelector('#profile-image');
 
 /* Step 4 - Adding Content */
 
