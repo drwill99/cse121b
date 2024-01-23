fe88b30bbb02aca216e03fd66f191e88535915ae
Dallin Williams <drw98@byui.edu>
2024-01-23 11:00:26 -0700

    added equations and HTML modifiers

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -3,17 +3,79 @@
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
 
+function add (number1, number2) {
+
+    return number1 + number2;
+
+}
+
+function addNumbers() {
+
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+
+    document.querySelector('#sum').value = add(addNumber1, addNumber2);
+}
+
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 
 /* Function Expression - Subtract Numbers */
 
+function subtract (number1, number2) {
+
+    return number1 - number2;
+
+}
+
+function subtractNumbers() {
+
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+}
+
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
 
 /* Arrow Function - Multiply Numbers */
 
+function multiply (number1, number2) {
+
+    return number1 * number2;
+
+}
+
+function multiplyNumbers() {
+
+    let multiplyNumber1 = Number(document.querySelector('#multiply1').value);
+    let multiplyNumber2 = Number(document.querySelector('#multiply2').value);
+
+    document.querySelector('#product').value = multiply(multiplyNumber1, multiplyNumber2);
+
+}
+
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
 
+function divide (number1, number2) {
+    
+        return number1 / number2;
+    
+    }
+    
+function divideNumbers() {
+    
+        let divideNumber1 = Number(document.querySelector('#divide1').value);
+        let divideNumber2 = Number(document.querySelector('#divide2').value);
+    
+        document.querySelector('#quotient').value = divide(divideNumber1, divideNumber2);
+    }
+
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
 
 /* Decision Structure */
+
 
 
 /* ARRAY METHODS - Functional Programming */
